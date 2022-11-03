cc1e09b2e44b5438ccf86a950a3534ee94fa6935
UngsterHamdung <0123ox@naver.com>
2022-11-03 16:53:58 +0900

    know

--- app/src/main/java/com/example/wristsample/MainActivity.java
+++ app/src/main/java/com/example/wristsample/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
@@ -13,20 +14,25 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.bluetooth.BluetoothSocket;
+
 import java.util.Set;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton saveBtn, bluetoothBtn;
+    private ImageView saveBtn, bluetoothBtn;
     private TextView statusText;
     private Set<BluetoothDevice> mPairedDevices;
+
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,28 +42,31 @@ public class MainActivity extends AppCompatActivity {
         saveBtn = findViewById(R.id.save);
         bluetoothBtn = findViewById(R.id.bluetooth);
         statusText = findViewById(R.id.status);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
                 if (mBluetoothAdapter == null) {
                     Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
                 } else {
                     if (mBluetoothAdapter.isEnabled()) {
                         Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
-                        statusText.setText("활성화");
+                        statusText.setText("블루투스 활성화");
                     } else {
                         Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
                     }
                 }
-//                if (mBluetoothAdapter.isEnabled()) {
-//                    mPairedDevices = mBluetoothAdapter.getBondedDevices();
-//                    if (mPairedDevices.size() > 0)
-//                    {
-//                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                        builder.setTitle("장치 선택");
-//                    }
-//                }
+                if (mBluetoothAdapter.isEnabled()) {
+                    selectBluetoothDevice();
+                }
+            }
+
+            void selectBluetoothDevice() {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mPairedDevices = mBluetoothAdapter.getBondedDevices();
             }
         });
     }
